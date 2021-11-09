d22a906c3521a952ab72b2515c82cd5c38f4b9d8
ivan schlagheck <ivansch05@gmail.com>
2021-11-09 16:04:11 +0100

    almost finished

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,51 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = Task.all
+  end 
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path(@task)
+  end
+
+  def edit
+  end
+
+  def update
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task.destroy
+  end
+  
+  private
+  
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task
+    @task= Task.find(params[:id])
+  end
+
+  def completed
+    @completed = @task.completed?
+    if @completed == false
+      return @completed = "This task is not completed yet"
+    else
+      return @completed = "This task is completed"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,26 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'index', to: "tasks#inde"
+  resources :tasks
+  # First we need to deply a form where the new data of a task will be inserted.
+  # get 'tasks/new', to: "tasks#new", as: :new_task
+
+  # # My Tasks page where all the tasks are.
+  # get 'tasks', to: "tasks#index", as: :tasks
+
+  # # Second the POST request to submit the form
+  # post 'tasks', to: "tasks#create"
+
+  # # Shows the details of an specific task
+  # get 'tasks/:id', to: "tasks#show", as: :task
+
+  # # Getting the task to update it
+  # get 'tasks/:id/edit', to: "tasks#edit", as: :edit_task
+
+  # # Submiting the update with a patch request
+  # patch 'tasks/:id', to: "tasks#update"
+
+  # # Deleting a task
+  # delete 'tasks/:id', to: "tasks#destroy"
+
+
 end
